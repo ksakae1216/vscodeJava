ad730b965f87b81f66aed2453179e64260a535f0
kotaro_sakae <ksakae1216@gmail.com>
2020-03-08 18:30:45 +0900

    second commit

--- src/main/java/jp/org/web/controller/IndexController.java
+++ src/main/java/jp/org/web/controller/IndexController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class IndexController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
 	/**
@@ -23,7 +23,7 @@ public class IndexController {
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("index screen display");
-		
+
 		return "index";
 	}
 }
